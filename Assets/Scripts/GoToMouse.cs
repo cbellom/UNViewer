6b7c71b17328f8f407dd704588ba9168196e939d
cbellom <cbellom@unal.edu.co>
2015-05-18 12:34:57 -0500

    init

---
+++ Assets/Scripts/GoToMouse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToMouse : MonoBehaviour {
+	public float speed;
+	public Vector3 target;
+	public Vector3 start;
+	private Vector3 position;
+	// Use this for initialization
+	void Start () {
+		start = transform.position;
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = Input.mousePosition;
+		position.z = 10;
+		Camera.main.ScreenToWorldPoint (position);
+
+		transform.position = Vector3.Lerp (transform.position, position, speed * Time.deltaTime);
+	}
+}
