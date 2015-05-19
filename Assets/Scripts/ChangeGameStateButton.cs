0242a8d5bb392b03a0da0f0ba14120245ddb03fc
cbellom <cbellom@unal.edu.co>
2015-05-19 12:42:49 -0500

    definicion de estados del juego menu

---
+++ Assets/Scripts/ChangeGameStateButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeGameStateButton : ObjectInteractable {
+
+	public GameState state;
+	public Utilities utilities;
+
+	void Awake(){
+		ObjectSelected = HandledObjectSelected;
+	}
+	
+	private void HandledObjectSelected (){
+		utilities.gameState = state;
+	}
+}
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -2,16 +2,30 @@
 using System.Collections;
 
 public class Utilities : MonoBehaviour {
+	public GameObject menuView;
+	public GameObject mainView;
+	public GameState gameState;
 
-	// Use this for initialization
 	void Start () {
 		Screen.showCursor = false;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			Application.Quit();
 		}
+
+		if (gameState == GameState.Main) {
+			mainView.SetActive(true);
+			StartCoroutine(CloseView(menuView));
+		} else if (gameState == GameState.Main) {
+			mainView.SetActive(false);
+			menuView.SetActive(true);
+		}
+	}
+
+	private IEnumerator CloseView(GameObject view){
+		yield return new WaitForSeconds(2);
+		view.SetActive (false);
 	}
 }
