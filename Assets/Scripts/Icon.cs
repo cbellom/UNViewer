32ec665feb2fa927fc12fc207f428c6e06d7490e
cbellom <cbellom@unal.edu.co>
2015-05-18 21:20:31 -0500

    data popup

--- Assets/Scripts/Icon.cs
+++ Assets/Scripts/Icon.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Icon : ObjectInteractable {
 	public GameObject informationView;
 	public Information information;
+
+	public GameObject dataList;
+	public GameObject dataPrefab;
 	
 	void Awake(){
 		ObjectSelected = HanldeObjectSelected;
@@ -30,6 +34,9 @@ public class Icon : ObjectInteractable {
 		preTitleText.text = information.preTitle;
 		Text titleText = GetChildOfPrefabByName ("Title").GetComponent<Text> () as Text;
 		titleText.text = information.title;
+
+		CreateDataList (information.dataList);
+
 		Text descriptionText = GetChildOfPrefabByName ("Description").GetComponent<Text> () as Text;
 		descriptionText.text = information.description;
 		Image pictureOf = GetChildOfPrefabByName ("Photo").GetComponent<Image> () as Image;
@@ -38,5 +45,20 @@ public class Icon : ObjectInteractable {
 
 	private Transform GetChildOfPrefabByName (string name)	{
 		return informationView.gameObject.transform.FindChild (name);
+	}
+
+	private void CreateDataList(List<Data> dataList){
+		EraseDataList ();
+		foreach (Data data in dataList) {
+			GameObject newItem = Instantiate(dataPrefab) as GameObject;
+			newItem.transform.SetParent(this.dataList.gameObject.transform, false);
+			newItem.GetComponent<Text>().text = "<color=#6fa15c>"+ data.type+"</color> <color=#000000>"+data.value+"</color>";
+		}
+	}
+
+	private void EraseDataList(){
+		foreach (Transform child in dataList.transform) {
+			Destroy (child.gameObject);
+		}
 	}
 }
