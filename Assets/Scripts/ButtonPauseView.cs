494a5d8b5ec1319c4283a1d4c14ce426b14b2551
cbellom <cbellom@unal.edu.co>
2015-06-06 18:19:12 +0200

    se crea ventana de pause

---
+++ Assets/Scripts/ButtonPauseView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPauseView : ObjectInteractable {
+	
+	public GameState state;
+	public GameObject pauseView;
+	public GameObject scrollMap;
+	public Utilities utilities;
+	
+	void Awake(){
+		ObjectSelected = HandledObjectSelected;
+	}
+	
+	private void HandledObjectSelected (){
+		if (scrollMap != null) {
+			scrollMap.SetActive(true);
+		}
+		if (pauseView != null) {
+			pauseView.SetActive (false);
+		}
+		utilities.gameState = state;
+	}
+}
---
+++ Assets/Scripts/MouseSleepListener.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MouseSleepListener : MonoBehaviour {
+	public float maxIdleTime = 5;
+	private float timeCount = 0;
+	private Vector3 lastMousePosition = Vector3.zero;
+	public GameObject pauseView;
+	public GameObject scrollMap;
+	public Utilities utilities;
+	// Update is called once per frame
+	void Update () {
+		if (utilities.gameState == GameState.Main) {
+		
+			if (lastMousePosition == Input.mousePosition) {
+				timeCount += Time.deltaTime;
+				if (timeCount > maxIdleTime) {
+					ActivePauseView();
+				}
+			} else {
+				timeCount = 0;
+			}
+		}
+
+		lastMousePosition = Input.mousePosition;
+	}
+
+	private void ActivePauseView(){
+		if (scrollMap != null) {
+			scrollMap.SetActive(false);
+		}
+		if (pauseView != null) {
+			pauseView.SetActive (true);
+		}
+	}
+}
